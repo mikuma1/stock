a57d8c76bb9a13abf7ceb84a8224a1ec0bc126f0
mikuma1 <72071661+mikuma1@users.noreply.github.com>
2020-10-30 18:46:25 +0900

    管理者権限の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
 
   def edit
   end
@@ -9,6 +12,17 @@ class UsersController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def admin
+    user = User.find(params[:id])
+    if user.admin
+      user.update(admin: false)
+    else
+      user.update(admin: true)
+    end
+    item = User.find(params[:id])
+    render json:{ user: item }
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  root to: 'item#index'
+  root to: 'items#index'
   devise_for :users
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:edit, :update, :index]
+  get "users/:id", to: 'users#admin'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
