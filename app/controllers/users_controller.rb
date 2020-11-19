167b3d6812a8812663c6797a92588fb0bd167ada
mikuma1 <72071661+mikuma1@users.noreply.github.com>
2020-11-19 19:19:30 +0900

    ユーザー削除機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
+<<<<<<< Updated upstream
+=======
+  before_action :set_organization, expect: :destroy
+
+  def index
+    @users = User.where(organization_id: params[:id])
+  end
+>>>>>>> Stashed changes
 
   def edit
   end
@@ -11,6 +19,27 @@ class UsersController < ApplicationController
     end
   end
 
+<<<<<<< Updated upstream
+=======
+  def destroy
+
+    user = User.find(params[:id])
+    user.delete
+    redirect_to users_path(id: user.organization_id)
+  end
+
+  def admin
+    user = User.find(params[:id])
+    if user.admin
+      user.update(admin: false)
+    else
+      user.update(admin: true)
+    end
+    item = User.find(params[:id])
+    render json:{ user: item }
+  end
+
+>>>>>>> Stashed changes
   private
 
   def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
   root to: 'item#index'
   devise_for :users
+<<<<<<< Updated upstream
   resources :users, only: [:edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+=======
+  resources :users, only: [:edit, :update, :index, :destroy]
+  get "users/:id", to: 'users#admin'
+  resources :organizations, only: [:new, :create] do
+    resources :items
+  end
+  get  '/login', to: 'login#new'
+  post '/login', to: 'login#create'
+  delete '/logout', to: 'login#destroy'
+>>>>>>> Stashed changes
 end
