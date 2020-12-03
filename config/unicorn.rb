fdb0842c87aa1cd76bcde7e8b8fd7cbbdcdebd5f
mikuma1 <72071661+mikuma1@users.noreply.github.com>
2020-12-03 14:20:40 +0900

    unicornの修正

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,6 +1,5 @@
 #サーバ上でのアプリケーションコードが設置されているディレクトリを変数に入れておく
-app_path = File.expand_path('../../', __FILE__)
-ENV['BUNDLE_GEMFILE'] = app_path + "/Gemfile"
+app_path ="/var/www/stock"
 
 #アプリケーションサーバの性能を決定する
 worker_processes 1
