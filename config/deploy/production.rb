def7ccd6fdb0d1bbb3ff878c7d07e6a711571d3f
mikuma1 <72071661+mikuma1@users.noreply.github.com>
2020-12-03 17:03:59 +0900

    サーバーの修正

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,4 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server '54.199.117.224', user: 'ec2-user', roles: %w{app db web}
+server 'stock-app.work', user: 'ec2-user', roles: %w{app db web}
