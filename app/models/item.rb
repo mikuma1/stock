4507487c16977408bf8c94164d9395fd00a0d6d1
mikuma1 <72071661+mikuma1@users.noreply.github.com>
2020-12-07 18:43:48 +0900

    categoryモデルの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,13 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
   has_one_attached :image
   belongs_to :organization
   has_many :consumptions
 
   with_options presence: true do
-    validates :category_id, :image
+    validates :image
+    validates :category_id, numericality: { other_than: 0 } 
     validates :url, length: { maximum: 300 }
     with_options length: { maximum: 40 } do
       validates :name
