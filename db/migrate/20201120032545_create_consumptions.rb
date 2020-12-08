c66cf3c07d131aa8171a4478491ff71ee5c1a1e4
mikuma1 <72071661+mikuma1@users.noreply.github.com>
2020-12-08 19:28:30 +0900

    approvalカラムの追加

--- db/migrate/20201120032545_create_consumptions.rb
+++ db/migrate/20201120032545_create_consumptions.rb
@@ -2,6 +2,7 @@ class CreateConsumptions < ActiveRecord::Migration[6.0]
   def change
     create_table :consumptions do |t|
       t.integer :quantity,  null: false
+      t.boolean :approval,  null: false, default: false
       t.references :item,   null: false, foreign_key: true
       t.references :user,   null: false, foreign_key: true
       t.timestamps
